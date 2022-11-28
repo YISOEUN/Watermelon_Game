7edb33845cd93edd1fd0ff639e4315567d4c3cd6
YISOEUN <65495429+YISOEUN@users.noreply.github.com>
2022-11-28 18:01:05 +0900

    Modify command

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -8,37 +8,35 @@ let extraScore = 1;
 // 无敌模式：true 改为 false
 let wuDi = true;
 
-// 第一个水果：修改数字为 0-10, 0 为葡萄，9 为半个西瓜
+// 게임을 시작할 떄 가장 먼저 떨어지는 과일을 지정할 수 있음 (기본: 0번과일)
 let firstFruit = 0;
 
-// 水果合成反转：false 改为 true
+// 리버스 옵션 설정 가능 (true: 큰 과일 -> 작은과일 / false: 작은과일 -> 큰과일)
 let reverseLevelUp = false;
 
-// 指定生成的水果：默认值: 不开启反转 0-5 开启反转 6-11，修改对应数字即可控制随机生成的水果范围
+// 떨어지는 과일의 범위를 지정
 const minRandomFruitNum = reverseLevelUp ? 6 : 0; // 生成随机水果最小值（0-10）0 为葡萄，9 为半个西瓜
 const maxRandomFruitNum = reverseLevelUp ? 11 : 5; // 生成随机水果最大值（1-11）0 为葡萄，9 为半个西瓜
 let setFruits = {
-  // 指定前几次生成的水果，可填入任意数量的数字，0 为葡萄，9 为半个西瓜
+  // 게임 시작 후 7회까지 떨어지는 과일의 조정 가능
   startFruits: reverseLevelUp ? [10, 10, 9, 8, 8, 7] : [0, 0, 1, 2, 2, 3],
   randomFunction: () => {
     return minRandomFruitNum + Math.floor(Math.random() * (maxRandomFruitNum - minRandomFruitNum));
   }
 }
 
-// 让水果更 Q 弹：false 改为大于 0 小于 1 的任意小数（推荐 0.9）
+// 떨어지는 과일의 물리법칙을 설정할 수 있음 ( 0~1까지 조절 가능)
+// 0.9 등 소숫점 포함
 let fruitQTan = false;
 
-// 让水果下落缓慢：false 改为大于 0 的任意数，值越大阻力越大，下落越慢（推荐 5）
+// 떨어지는 고일의 속도를 조절할 수 있음 ( 0~5까지)
 let fruitSlowDown = false;
 
-// 点击右上方图标更换水果：false 改为 true 即可
+// 오른쪽 버튼을 클릭할 때 과일을 바꿀 수 있는지 여부를 설정(false: 교체불가 / true: 교체가능)
 let clickChangeFruit = true;
 
-// 广告链接：false 或为空字符串表示不会跳转到广告
-let adLink = 'https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/yupi_wechat.png';
-
-// 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
+// 게임 사이트의 홈페이지 제목을 지정할 수 있음
+document.getElementsByTagName("title")[0].innerText = '선영 바보';
 
 // 开启选分弹窗：将 false 改为 true
 let selectModal = false;
